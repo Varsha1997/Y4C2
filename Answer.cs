1d8b4af5718f4dae8971eb1492f9831f98ef873b
nahjani1 <41770891+nahjani1@users.noreply.github.com>
2019-04-09 14:36:29 -0400

    New Changes to Survey

---
+++ Answer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Y4C2.Models
+{
+    public class Answer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Response { get; set; }
+        [ForeignKey("Questions")]
+        public int QuestionId { get; set; }
+
+        public virtual Questions Question { get; set; }
+    }
+}
---
+++ Survey.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Y4C2.Models
+{
+    public class Survey
+    {
+        /**
+        [Key]
+        public int Id { get; set; }
+        public string question { get; set; }
+        public string answer { get; set; }
+        public string answerType { get; set; }
+        public string user { get; set; }
+        public string theme { get; set; }
+        */
+
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Account")]
+        public int UserId { get; set; }
+
+        [ForeignKey("AC")]
+        public int ThemeId { get; set; }
+        
+        public IList<Questions> Question { get; set; }
+        public AddContent Themes  { get; set; }
+        //public IList<Account> Users { get; set; }//
+    }
+}
