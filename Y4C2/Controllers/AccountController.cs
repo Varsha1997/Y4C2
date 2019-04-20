435bf563455d29202b2d9ceb3a0d60cb0b3b6fcc
Darnell Lemons <darnelldlemons@gmail.com>
2019-04-20 09:24:45 -0400

    Deleted Comments

--- Y4C2/Controllers/AccountController.cs
+++ Y4C2/Controllers/AccountController.cs
@@ -133,7 +133,7 @@ namespace Y4C2.Controllers
                 }
             }
 
-            TempData["users"] = "usersmynigga";
+            
             ViewBag.themes = themeCount;
             TempData["themes"] = themeCount;
             ViewBag.active = activeThemes;
